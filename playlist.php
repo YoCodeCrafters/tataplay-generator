a4770536c10959f920852585c190ad17a5114d8a
Sabbir Hasan <sabbirhasan10000bd@gmail.com>
2024-01-31 20:42:55 +0600

    update: filename changed

--- playlist.php
+++ playlist.php
@@ -11,7 +11,7 @@ $m3u_content = '#EXTM3U x-tvg-url="https://www.tsepg.cf/epg.xml.gz"' . "\n\n";
 foreach ($data as $channel) {
     $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
 $base_path = rtrim(dirname($_SERVER['PHP_SELF']), '/');
-$license_url = "{$protocol}://{$_SERVER['HTTP_HOST']}" . ($base_path ? $base_path : '') . "/license.php?id={$channel['channel_id']}";
+$license_url = "{$protocol}://{$_SERVER['HTTP_HOST']}" . ($base_path ? $base_path : '') . "/wv-license.php?id={$channel['channel_id']}";
     $m3u_content .= "#EXTINF:-1 tvg-id=\"{$channel['channel_id']}\" tvg-logo=\"https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/{$channel['channel_logo']}\" group-title=\"{$channel['channel_genre']}\",{$channel['channel_name']}\n";
     $m3u_content .= "#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha\n";
     $m3u_content .= "#KODIPROP:inputstream.adaptive.license_key={$license_url}\n";

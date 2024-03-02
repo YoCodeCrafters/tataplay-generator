839c8ae30377a74b9ddf94bd118fa93e129e8a5f
harshitrathi28 <155360887+harshitrathi28@users.noreply.github.com>
2024-03-02 12:37:48 +0530

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -14,7 +14,7 @@ $base_path = rtrim(dirname($_SERVER['PHP_SELF']), '/');
 $license_url = "{$protocol}://{$_SERVER['HTTP_HOST']}" . ($base_path ? $base_path : '') . "/wv-license.php?id={$channel['channel_id']}";
     $m3u_content .= "#EXTINF:-1 tvg-id=\"{$channel['channel_id']}\" tvg-logo=\"https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/{$channel['channel_logo']}\" group-title=\"{$channel['channel_genre']}\",{$channel['channel_name']}\n";
     $m3u_content .= "#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha\n";
-    $m3u_content .= "#KODIPROP:inputstream.adaptive.license_key={$license_url}\n";
+    $m3u_content .= "#KODIPROP:inputstream.adaptive.license_key="{$channel['channel_license_url']}" {$license_url}\n";
     $m3u_content .= "{$channel['channel_url']}\n\n";
 }
 
